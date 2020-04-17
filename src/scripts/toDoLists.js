c5baccb4c4f1117bc913a5160fdf910e1dd274bd
Szymon Hrabia <survikrowa@o2.pl>
2020-04-17 09:20:53 +0200

    Implement html structure for toDoLists

--- src/scripts/toDoLists.js
+++ src/scripts/toDoLists.js
@@ -1,2 +1,28 @@
 export const toDoLists = `
-<h1>Hello on ToDoLists</h1>`;
+<header>
+    <h1>Main Panel</h1>
+</header>
+<main>
+    <section>
+    <h2>Task lists</h2>
+        <div>
+            <span>Hello world</span>
+            <button>Delete me</button>
+            <fieldset>
+                <label for="js-new-task" >Enter your task name:</label>
+                <input type="text" id="js-new-task" name="task">
+             </fieldset>
+            <button>Add new Task</button>
+            <div>
+                <span>Your tasks:</span>
+            </div>
+        </div>
+    </section>
+    <section>
+        <fieldset>
+            <label for="js-new-task-list" >Enter your task list name:</label>
+            <input type="text" id="js-new-task-list" name="task-list">
+        </fieldset>
+        <button type="button">Add Task-List</button>
+    </section>
+</main>`;
