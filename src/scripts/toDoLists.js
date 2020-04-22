1b5eff021e6825552fb13e24477def2dc0b47ff4
Szymon Hrabia <survikrowa@o2.pl>
2020-04-22 22:59:36 +0200

    Refactor: Delete useless code

--- src/scripts/toDoLists.js
+++ src/scripts/toDoLists.js
@@ -1,28 +1,17 @@
 export const toDoLists = `
 <header>
-    <h1>Main Panel</h1>
+    <h1 class="header__h1">Main Panel</h1>
 </header>
-<main>
+<main class="toDoList">
     <section>
-    <h2>Task lists</h2>
-        <div>
-            <span>Hello world</span>
-            <button>Delete me</button>
-            <fieldset>
-                <label for="js-new-task" >Enter your task name:</label>
-                <input type="text" id="js-new-task" name="task">
-             </fieldset>
-            <button>Add new Task</button>
-            <div>
-                <span>Your tasks:</span>
-            </div>
-        </div>
+    <h2 class="toDoList__header">Task lists</h2>
+        <div id="js-task-list-target"></div> 
     </section>
-    <section>
-        <fieldset>
+    <section class="toDoList__addTask" >
+        <fieldset class="toDoList__fieldset">
             <label for="js-new-task-list" >Enter your task list name:</label>
-            <input type="text" id="js-new-task-list" name="task-list">
+            <input type="text" id="js-new-task-list" name="task-list" class="toDoList__input"  value="" />
         </fieldset>
-        <button type="button">Add Task-List</button>
+        <button type="button" class="btn btn__primary" id="js-add-new-task-list">Add Task-List</button>
     </section>
 </main>`;
