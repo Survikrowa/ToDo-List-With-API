39cdd31f47686215cc6cf423372991810707f990
Survikrowa <szymonhrabia60@gmail.com>
2020-12-03 20:44:37 +0100

    begin app rewrite to React

--- src/scripts/renderToDoTask.js
+++
@@ -1,7 +0,0 @@
-export const toDoTask = taskName => {
-  return `
-    <div data-id="${taskName.id}" class="task">
-        <span>${taskName.description}</span>
-        <button class="js-delete-task-btn">Delete Task</button>
-    </div>`;
-};
