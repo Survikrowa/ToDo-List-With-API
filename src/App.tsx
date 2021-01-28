d3266dc413296caacf8f34d058c49421a3cfc57d
survikrowa <szymonhrabia60@gmail.com>
2021-01-28 11:59:12 +0100

    Add authprovider and new private route to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
-import { FormContainer } from "./components/forms/FormContainer";
+import { Main } from "./pages/main/Main";
+import { AuthProvider } from "./context/authContext";
+import { PrivateRoute } from "./components/routes/PrivateRoute";
 
 export const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route path="/">
-          <FormContainer />
-        </Route>
-      </Switch>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Main />
+          </Route>
+          <PrivateRoute path="/dashboard">
+            <div>Witam</div>
+          </PrivateRoute>
+        </Switch>
+      </Router>
+    </AuthProvider>
   );
 };
